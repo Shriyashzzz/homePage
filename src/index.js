56ebea9a99b08e248e9e7b515fc5c0549c7faf34
Shriyash <ghimires1@my.smccd.edu>
2026-05-11 04:48:50 -0700

    has loader

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import "./style.css";
 
 import "./bobTheblob.js";
 import "./nav.js";
+
+window.onload = () => {
+  const loader = document.querySelector(".loader");
+  const content = document.querySelector(".Content");
+  setTimeout(() => {
+    content.style.display = "block";
+    loader.style.display = "none";
+  }, 1000);
+};
