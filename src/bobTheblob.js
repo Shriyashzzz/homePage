8a2717386253dc2366ab0844e98c8699d018f15e
Shriyash <ghimires1@my.smccd.edu>
2026-05-11 12:16:26 -0700

    smooth scroll state/ bob blush effect added

--- src/bobTheblob.js
+++ src/bobTheblob.js
@@ -3,7 +3,48 @@
 
   const cursorBall = document.getElementById("cursorFollow");
   window.addEventListener("mousemove", (event) => {
-    cursorBall.style.transform = `translate(${event.clientX}px ,${event.clientY}px) scale(${isHoverd ? 1.5 : 1})`;
-    cursorBall.style.backgroundColor = isHoverd ? "red" : `rgb(141, 255, 47)`;
+    cursorBall.style.transform = `translate(${event.clientX}px ,${event.clientY}px) scale(${isHoverd ? 1.2 : 1})`;
+    cursorBall.style.backgroundColor = isHoverd
+      ? "#92AA83"
+      : `rgb(141, 255, 47)`;
+  });
+
+  const menuTitle = document.querySelector(".menu-title");
+  menuTitle.addEventListener("mouseenter", () => {
+    isHoverd = true;
+  });
+
+  menuTitle.addEventListener("mouseleave", () => {
+    isHoverd = false;
+  });
+
+  const navLink = document.querySelectorAll(".navLink");
+  navLink.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+      isHoverd = true;
+    });
+    link.addEventListener("mouseleave", () => {
+      isHoverd = false;
+    });
+  });
+
+  const projects = document.querySelectorAll(".project");
+  projects.forEach((proj) => {
+    proj.addEventListener("mouseenter", () => {
+      isHoverd = true;
+    });
+    proj.addEventListener("mouseleave", () => {
+      isHoverd = false;
+    });
+  });
+
+  const icons = document.querySelectorAll(".svgIcons");
+  icons.forEach((icon) => {
+    icon.addEventListener("mouseenter", () => {
+      isHoverd = true;
+    });
+    icon.addEventListener("mouseleave", () => {
+      isHoverd = false;
+    });
   });
 })();
