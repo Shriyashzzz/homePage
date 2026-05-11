ca71648b70f720584043375ab89b9c093e128966
Shriyash <ghimires1@my.smccd.edu>
2026-05-11 15:22:07 -0700

    final changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./style.css";
 import "./bobTheblob.js";
 import "./nav.js";
 
-window.onload = () => {
+window.addEventListener("DOMContentLoaded", () => {
   const loader = document.querySelector(".loader");
   const content = document.querySelector(".Content");
   setTimeout(() => {
     content.style.display = "block";
     loader.style.display = "none";
   }, 1200);
-};
+});
