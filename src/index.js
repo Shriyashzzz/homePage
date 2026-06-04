e1c0010d1514b39365c6e6c2a8cf6f7ce6a410bf
Shriyash <ghimires1@my.smccd.edu>
2026-06-04 14:15:35 -0700

    chore: add vercel analytics script

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import "./style.css";
 
 import "./bobTheblob.js";
 import "./nav.js";
+import { inject } from "@vercel/analytics";
 
+inject();
 window.addEventListener("DOMContentLoaded", () => {
   const loader = document.querySelector(".loader");
   const content = document.querySelector(".Content");
